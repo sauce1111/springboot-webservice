cc0477665e3d7398a9360d629013f530a21c4b7c
sauce1111 <sauce0127@gmail.com>
2020-06-28 21:10:08 +0900

    JPA Auditing 으로 createDate/modifiedDate 자동 생성 추가

--- src/test/java/com/sauce/book/springboot/domain/PostsRepositoryTest.java
+++ src/test/java/com/sauce/book/springboot/domain/PostsRepositoryTest.java
@@ -1,7 +1,10 @@
-package com.sauce.book.springboot.domain.posts;
+package com.sauce.book.springboot.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.sauce.book.springboot.domain.posts.Posts;
+import com.sauce.book.springboot.domain.posts.PostsRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -42,6 +45,32 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(
+            ">>>>>>>>>>>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts
+                .getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+
     }
 
 }
