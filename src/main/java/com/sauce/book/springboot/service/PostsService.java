026a084eab0e155c232ac4a123d84566458eb89d
sauce1111 <sauce0127@gmail.com>
2020-06-28 23:10:02 +0900

    mustache 추가, index 페이지 생성, 글 등록 및 목록 조회 기능 추가

--- src/main/java/com/sauce/book/springboot/service/PostsService.java
+++ src/main/java/com/sauce/book/springboot/service/PostsService.java
@@ -2,12 +2,15 @@ package com.sauce.book.springboot.service;
 
 import com.sauce.book.springboot.domain.posts.Posts;
 import com.sauce.book.springboot.domain.posts.PostsRepository;
+import com.sauce.book.springboot.web.dto.PostsListResponseDto;
 import com.sauce.book.springboot.web.dto.PostsResponseDto;
 import com.sauce.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sauce.book.springboot.web.dto.PostsUpdateRequestDto;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -28,9 +31,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다.id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
 }
