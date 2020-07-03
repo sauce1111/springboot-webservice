b70751fd94865d438bbadfa2c6da348085677cb4
sauce1111 <sauce0127@gmail.com>
2020-07-04 04:50:49 +0900

    코드 변경 사항 반영 안되고 있었음

---
+++ src/main/java/com/sauce/book/springboot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sauce.book.springboot.config;
+
+import com.sauce.book.springboot.config.auth.LoginUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
---
+++ src/main/java/com/sauce/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.sauce.book.springboot.config.auth.dto;
+
+import com.sauce.book.springboot.domain.user.User;
+import java.io.Serializable;
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
